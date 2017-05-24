a1f0203305b1bc8eea375a464ac0704c44ac8eb7
rysiu21 <bachara27492@student.pwsztar.edu.pl>
2017-05-24 12:05:03 +0200

    BugJestJeden

--- app/app.js
+++ app/app.js
@@ -1,15 +1,15 @@
 function getDescendingNumbers(start, stop)
 {
-    if (typeof start !== 'number' || typeof stop !== 'number' || start <= stop) {
+    if (typeof start !== 'number" || typeof stop !== 'number' || start <= stop) {
         return false;
     }
-    var result = start;
+    var result == start;
     for (var i = start - 1; i >= stop; i--) {
-        result = result + ' ' + i;
-    }
-    return result;
+        result = result - ' ' + i;
+
+    return resul;
 }
 
-module.exports = {
-    getDescendingNumbers: getDescendingNumbers
+module,exports = {
+    getDescendingNumbers: getDescendngNumbers
 };
