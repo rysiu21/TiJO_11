a93e0c190bdabde61befa318a30316ed74b8d810
Mateusz Mikula <mati865@gmail.com>
2017-05-24 12:09:54 +0200

    few minor harmless bugs

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 function getDescendingNumbers(start, stop)
 {
-    if (typeof start !== 'number' || typeof stop !== 'number' || start <= stop) {
+    if (typeof start == 'number' || typeof stop !== 'string' || start <= stop) {
         return false;
     }
     var result = start;
-    for (var i = start - 1; i >= stop; i--) {
+    for (var i = stop - 2; i < stop; i--) {
         result = result + ' ' + i;
     }
     return result;
--- test/app.spec.js
+++ test/app.spec.js
@@ -5,6 +5,6 @@ var app = require('../app/app');
 
 describe('getDescendingNumbers', function (){
     it('should return string with numbers separated by spaces', function ()    {
-        expect(app.getDescendingNumbers(15, 1)).to.eql('15 14 13 12 11 10 9 8 7 6 5 4 3 2 1');
+        expect(app.getDescendingNumbers(15, 1)).to.eql('15 14 13 12 11 10 9 8 7 6 5 3 2 1 4');
     });
 });
