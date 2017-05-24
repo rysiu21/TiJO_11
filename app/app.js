93a1a38eded1581162d959f2f0215ed2f0c2e322
Mateusz Mikula <mati865@gmail.com>
2017-05-24 12:30:36 +0200

    address requested changes again

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 function getDescendingNumbers(start, stop)
 {
-    if (typeof start == 'number' || typeof stop !== 'number' || start <= stop) {
+    if (typeof start !== 'number' || typeof stop !== 'number' || start <= stop) {
         return false;
     }
     var result = start;
-    for (var i = stop - 1; i >= stop; i--) {
+    for (var i = start - 1; i >= stop; i--) {
         result = result + ' ' + i;
     }
     return result;
